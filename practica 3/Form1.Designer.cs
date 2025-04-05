ac3b08b466f581edb2d9103b7e2a006815f33757
gaonacami <gaonacami071@gmail.com>
2025-04-04 21:32:49 -0500

    Add files via upload

---
+++ practica 3/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace practica_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(263, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(357, 19);
+            label1.TabIndex = 0;
+            label1.Text = "Este programa suma los numeros capturados";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(165, 104);
+            label2.Name = "label2";
+            label2.Size = new Size(169, 19);
+            label2.TabIndex = 1;
+            label2.Text = "Introduce un numero";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(165, 315);
+            label3.Name = "label3";
+            label3.Size = new Size(188, 19);
+            label3.TabIndex = 2;
+            label3.Text = "La suma de todos son: ";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(389, 102);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 25);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(389, 315);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 25);
+            textBox2.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = Color.MistyRose;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 17;
+            listBox1.Location = new Point(630, 109);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(109, 225);
+            listBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.MistyRose;
+            button1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(52, 297);
+            button1.Name = "button1";
+            button1.Size = new Size(107, 59);
+            button1.TabIndex = 6;
+            button1.Text = "Sumar numeros ";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.MistyRose;
+            button2.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(389, 154);
+            button2.Name = "button2";
+            button2.Size = new Size(86, 39);
+            button2.TabIndex = 7;
+            button2.Text = "Añadir";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private ListBox listBox1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ practica 3/Form1.cs
@@ -0,0 +1,28 @@
+namespace practica_3
+{
+    public partial class Form1 : Form
+    {
+        float f_num, Total = 0; 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Boton Sumarnumeros
+            textBox2.Text = Total.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Boton añadir
+            listBox1.Items.Add(textBox1.Text);
+            //Aqui es donde pasamos el parametro
+            f_num = float.Parse(textBox1.Text);
+            Total += f_num;
+            textBox1.Text = "";
+            textBox1.Focus(); 
+        }
+    }
+}
