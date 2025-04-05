1a5fd4d2305b461f4504c36524100cce899192c5
gaonacami <gaonacami071@gmail.com>
2025-04-04 21:24:23 -0500

    Add files via upload

---
+++ practica #5/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace practica__5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(375, 38);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(116, 19);
+            label1.TabIndex = 0;
+            label1.Text = "Area triangulo";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(125, 121);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(48, 19);
+            label2.TabIndex = 1;
+            label2.Text = "Base";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(125, 198);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(54, 19);
+            label3.TabIndex = 2;
+            label3.Text = "Altura";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(125, 286);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(44, 19);
+            label4.TabIndex = 3;
+            label4.Text = "Area";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(250, 114);
+            textBox1.Margin = new Padding(4, 3, 4, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(98, 26);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(250, 191);
+            textBox2.Margin = new Padding(4, 3, 4, 3);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(98, 26);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(250, 286);
+            textBox3.Margin = new Padding(4, 3, 4, 3);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(98, 26);
+            textBox3.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Location = new Point(375, 323);
+            button1.Margin = new Padding(4, 3, 4, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(107, 26);
+            button1.TabIndex = 7;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.AppWorkspace;
+            ClientSize = new Size(1143, 503);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+    }
+}
---
+++ practica #6/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace practica__6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            valor = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label4 = new Label();
+            label5 = new Label();
+            Resultado = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(110, 42);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 19);
+            label1.TabIndex = 0;
+            label1.Text = "Datos";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(150, 95);
+            label2.Name = "label2";
+            label2.Size = new Size(131, 19);
+            label2.TabIndex = 1;
+            label2.Text = "Ingresa el valor:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(341, 92);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 26);
+            textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(110, 171);
+            label3.Name = "label3";
+            label3.Size = new Size(195, 19);
+            label3.TabIndex = 3;
+            label3.Text = "Opciones de conversion\r\n";
+            // 
+            // valor
+            // 
+            valor.BackColor = SystemColors.ButtonShadow;
+            valor.Location = new Point(110, 218);
+            valor.Name = "valor";
+            valor.Size = new Size(121, 32);
+            valor.TabIndex = 4;
+            valor.Text = "FAHRENHEIT";
+            valor.UseVisualStyleBackColor = false;
+            valor.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ButtonShadow;
+            button2.Location = new Point(253, 218);
+            button2.Name = "button2";
+            button2.Size = new Size(142, 32);
+            button2.TabIndex = 5;
+            button2.Text = "CENTIGRADOS";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ButtonShadow;
+            button3.Location = new Point(437, 218);
+            button3.Name = "button3";
+            button3.Size = new Size(123, 32);
+            button3.TabIndex = 6;
+            button3.Text = "BORRAR";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(110, 307);
+            label4.Name = "label4";
+            label4.Size = new Size(168, 19);
+            label4.TabIndex = 7;
+            label4.Text = "Resultado en grados";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(205, 349);
+            label5.Name = "label5";
+            label5.Size = new Size(186, 19);
+            label5.TabIndex = 8;
+            label5.Text = "Equivalente en grados:";
+            // 
+            // Resultado
+            // 
+            Resultado.Location = new Point(341, 381);
+            Resultado.Name = "Resultado";
+            Resultado.Size = new Size(100, 26);
+            Resultado.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1143, 503);
+            Controls.Add(Resultado);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(valor);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private Button valor;
+        private Button button2;
+        private Button button3;
+        private Label label4;
+        private Label label5;
+        private TextBox Resultado;
+    }
+}
